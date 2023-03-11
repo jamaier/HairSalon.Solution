e9b568996a54cb3286c8b74b10f41d6806dff603
Jacob Maier <jacobamaier@gmail.com>
2023-03-10 16:40:16 -0800

    MySql Working with workaround.

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,8 +2,8 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    public int ClientId { get; set; }
-    public string ClientName { get; set; }
+    public int ClientId { get; set; } 
+    public string ClientName { get; set; } 
 
     public int StylistId { get; set; }
     public Stylist Stylist { get; set; }
