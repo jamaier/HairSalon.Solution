fbfc48527df3791481f79588916ced11634c63d7
Jacob Maier <jacobamaier@gmail.com>
2023-03-12 12:58:21 -0700

    Bugfix: Linked stylist details page.

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -6,11 +6,11 @@ using System.Linq;
 
 namespace HairSalon.Controllers
 {
-  public class stylistsController : Controller
+  public class StylistsController : Controller
   {
     private readonly HairSalonContext _db;
 
-    public stylistsController(HairSalonContext db)
+    public StylistsController(HairSalonContext db)
     {
       _db = db;
     }
